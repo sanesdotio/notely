611a5b1fda3c2975718cce46359e89f8a71e3768
sanesdotio <sander.neset.dev@gmail.com>
2025-09-08 20:01:50 +0200

    Fix linting issues

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,3 @@
-/* global process */
 import dotenv from "dotenv";
 dotenv.config();
 
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-/* global process */
 import express from "express";
 import cors from "cors";
 import path from "path";
